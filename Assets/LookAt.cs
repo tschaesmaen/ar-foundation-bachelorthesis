b3980a0113f8742ee6151e3182f8549d6c5be3ba
tschaesmaen <45597109+tschaesmaen@users.noreply.github.com>
2021-03-21 10:40:47 +0100

    AR App animated UI

---
+++ Assets/LookAt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+
+    public GameObject aRPlayer;
+
+    private void Awake()
+    {
+        aRPlayer = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    private void Update()
+    {
+        Vector3 targetPosition = new Vector3(aRPlayer.transform.position.x,
+                                             transform.position.y,
+                                             aRPlayer.transform.position.z);
+        transform.LookAt(targetPosition);
+    }
+
+}
---
+++ Assets/SessionStatusDisplayer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using TMPro;
+
+
+public class SessionStatusDisplayer : MonoBehaviour
+{
+    //Component to display information
+    private TextMeshProUGUI sessionStatusText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ARSession.stateChanged += HandleStateChanged;
+        sessionStatusText = this.GetComponent<TextMeshProUGUI>();
+    }
+
+    private void HandleStateChanged(ARSessionStateChangedEventArgs statEventArguments)
+    {
+        switch (statEventArguments.state)
+        {
+            case ARSessionState.None:
+                sessionStatusText.text = "Session Status: Unknown";
+                break;
+            case ARSessionState.Unsupported:
+                sessionStatusText.text = "Session Status: ARFoundation not supported";
+                break;
+
+            case ARSessionState.CheckingAvailability:
+                sessionStatusText.text = "Checking...";
+                break;
+            case ARSessionState.NeedsInstall:
+                sessionStatusText.text = "Needs Install";
+                break;
+            case ARSessionState.Installing:
+                sessionStatusText.text = "Installing";
+                break;
+            case ARSessionState.Ready:
+                sessionStatusText.text = "Ready";
+                break;
+            case ARSessionState.SessionInitializing:
+                sessionStatusText.text = "Poor SLAM Quality";
+                break;
+            case ARSessionState.SessionTracking:
+                sessionStatusText.text = "Tracking Quality is good";
+                break;
+            default:
+                sessionStatusText.text = "Session Status: Unknown";
+                break;
+
+
+        }
+    }
+}
